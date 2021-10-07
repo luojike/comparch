a705857284b8271b1e41ebf037132e012abc81b9
PinkGranite <53990884+PinkGranite@users.noreply.github.com>
2021-10-07 19:40:53 +0800

    Add files via upload

---
+++ 2021/S211000846-严宇威/matrix_mul.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<cstring>
+#include<Windows.h>
+constexpr auto MAT_SIZE = 2000;;
+using namespace std;
+
+int main() {
+	int (*mat)[MAT_SIZE] = new int[MAT_SIZE][MAT_SIZE];
+	int (*result)[MAT_SIZE] = new int[MAT_SIZE][MAT_SIZE];
+	memset(mat, -1, MAT_SIZE * MAT_SIZE * sizeof(int));
+	memset(result, 0, MAT_SIZE * MAT_SIZE * sizeof(int));
+	LARGE_INTEGER t1, t2, tc;
+	QueryPerformanceFrequency(&tc);
+	// funtion 1
+	QueryPerformanceCounter(&t1);
+	for (size_t i = 0; i < MAT_SIZE; i++)
+	{
+		for (size_t j = 0; j < MAT_SIZE; j++) {
+			for (size_t k = 0; k < MAT_SIZE; k++)
+			{
+				result[i][j] += mat[i][k] * mat[k][j];
+			}
+		}
+	}
+	QueryPerformanceCounter(&t2);
+	cout << "time1 = " << (double)(t2.QuadPart - t1.QuadPart) / (double)tc.QuadPart << endl;  // 23.6786
+
+	// function 2
+	memset(result, 0, MAT_SIZE * MAT_SIZE * sizeof(int));
+	QueryPerformanceCounter(&t1);
+	for (size_t j = 0; j < MAT_SIZE; j++)
+	{
+		for (size_t i = 0; i < MAT_SIZE; i++) {
+			for (size_t k = 0; k < MAT_SIZE; k++)
+			{
+				result[i][j] += mat[i][k] * mat[k][j];
+			}
+		}
+	}
+	QueryPerformanceCounter(&t2);
+	cout << "time2 = " << (double)(t2.QuadPart - t1.QuadPart) / (double)tc.QuadPart << endl;  // 21.2423
+
+	// function 3
+	memset(result, 0, MAT_SIZE * MAT_SIZE * sizeof(int));
+	QueryPerformanceCounter(&t1);
+	for (size_t k = 0; k < MAT_SIZE; k++)
+	{
+		for (size_t i = 0; i < MAT_SIZE; i++) {
+			for (size_t j = 0; j < MAT_SIZE; j++)
+			{
+				result[i][j] += mat[i][k] * mat[k][j];
+			}
+		}
+	}
+	QueryPerformanceCounter(&t2);
+	cout << "time3 = " << (double)(t2.QuadPart - t1.QuadPart) / (double)tc.QuadPart << endl;  // 16.9107
+}
