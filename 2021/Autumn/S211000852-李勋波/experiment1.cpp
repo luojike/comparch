76b6cc60edf2b60ca7e2af818e5b7d856d3eac0b
lannas <92204029+lannas@users.noreply.github.com>
2021-10-11 17:29:46 +0800

    Create a folder for experiment 1

---
+++ 2021/Autumn/S211000852-李勋波/experiment1.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+#include <cstdlib>
+#include <ctime> 
+#define n 1000
+#define _for(i,j,k) for(int i=j;i<k;i++)
+#define for_(i,j,k)	for(int i=k-1;i>=j;i--)
+using namespace std;
+int a[n][n];
+int b[n][n];
+int c[n][n];
+int main()
+{
+	int start,end;
+	srand((int)time(0));  // 产生随机种子
+    _for(i,0,n)
+    _for(j,0,n)
+    	{
+    	a[i][j]=rand()%20;
+		b[i][j]=rand()%20;	
+		}
+		
+	start=clock();	
+	_for(i,0,n)	
+	_for(j,0,n)
+	_for(k,0,n)
+	c[i][j]+=a[i][k]*b[k][j];
+	end=clock();
+	
+	cout<<"(ijk) time is: "<<(end-start)/1000.0<<"sec"<<endl;
+	
+	start=clock();
+	_for(i,0,n)	
+	_for(k,0,n)
+	_for(j,0,n)
+	c[i][j]+=a[i][k]*b[k][j];
+	end=clock();
+	cout<<"(ikj) time is: "<<(end-start)/1000.0<<"sec"<<endl;
+	
+	start=clock();
+	_for(j,0,n)
+	_for(i,0,n)	
+	_for(k,0,n)
+	c[i][j]+=a[i][k]*b[k][j];
+	end=clock();
+	cout<<"(jik) time is: "<<(end-start)/1000.0<<"sec"<<endl;
+	
+	start=clock();
+	_for(j,0,n)	
+	_for(k,0,n)
+	_for(i,0,n)
+	c[i][j]+=a[i][k]*b[k][j];
+	end=clock();
+	cout<<"(jki) time is: "<<(end-start)/1000.0<<"sec"<<endl;
+
+	start=clock();
+	_for(k,0,n)
+	_for(i,0,n)	
+	_for(j,0,n)
+	c[i][j]+=a[i][k]*b[k][j];
+	end=clock();
+	cout<<"(kij) time is: "<<(end-start)/1000.0<<"sec"<<endl;
+	
+	start=clock();
+	_for(k,0,n)
+	_for(j,0,n)	
+	_for(i,0,n)
+	c[i][j]+=a[i][k]*b[k][j];
+	end=clock();
+	cout<<"(kji) time is: "<<(end-start)/1000.0<<"sec";
+	
+	return 0;
+}
